205e0a4e5f1bb9f7d5c109fd76673ef28e27cfec
“zndos” <eu.akimov73@yandex.ru>
2021-02-15 17:33:52 +0300

    Add User

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,20 +2,20 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get home_url
     assert_response :success
 
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_url
     assert_response :success
 
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_url
     assert_response :success
-
   end
+
 end
